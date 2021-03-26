55d0b2b263c56ab9f4e96ef6a6972436c8257d21
Yurk Sha <yurkins@gmail.com>
2021-03-26 17:29:01 +0200

    add field.$value prop

--- src/field/update-helper.js
+++ src/field/update-helper.js
@@ -3,6 +3,7 @@ var fieldSetValue = require('./set-value')
 var fieldValidate = require('./validate')
 var fieldGetOptions = require('./get-options')
 var fieldDynamicType = require('./get-dynamic-type')
+var fieldGetValue = require('./get-value')
 
 module.exports = function (field, values, as_is) {
   fieldSetValue.call(this, field, values, as_is)
@@ -14,4 +15,6 @@ module.exports = function (field, values, as_is) {
   field.$type = (fieldDynamicType.call(this, field) || ((field.field_type === this.fieldtypes.text) && field.data_type || field.field_type)).toLowerCase()
 
   fieldValidate.call(this, field)
+
+  field.$value = fieldGetValue.call(this, field, 'label').join(field.multiline ? '<br>' : ', ')
 }

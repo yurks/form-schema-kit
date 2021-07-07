573d5f564ce4c915d6a0c049862f84d1fb6ae9e7
Yurk Sha <yurkins@gmail.com>
2021-07-07 16:56:53 +0300

    `predefined` flag for field options

--- src/field/initialize-value.js
+++ src/field/initialize-value.js
@@ -4,6 +4,8 @@ var fieldGetDefaultValues = require('./get-value-default')
 var fieldGetPrimary = require('./get-primary')
 var fieldGetValue = require('./get-value')
 var fieldUpdateHelper = require('./update-helper')
+var each = require('wsk-utils/var/object/each')
+var extend = require('wsk-utils/var/object/extend')
 
 var fieldInitializeValues = function (field, initial) {
   if (initial !== false) {
@@ -28,7 +30,15 @@ var fieldInitializeValues = function (field, initial) {
     fieldUpdateHelper.call(this, field, fieldGetDefaultValues.call(this, field))
   }
 
-  if (fieldGetValue.call(this, field, 'id')[0]) {
+  var values = fieldGetValue.call(this, field, 'id');
+
+  each(field.$options, function(i, opt) {
+    if (values.indexOf(opt.id) >= 0) {
+      field.$options[i] = extend({}, opt, { predefined: true })
+    }
+  })
+  
+  if (values[0]) {
     if (field.predefined == null) {
       field.predefined = true
     }

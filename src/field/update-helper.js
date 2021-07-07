4822570685e3846d2427fab19260752b24cc2cff
Yurk Sha <yurkins@gmail.com>
2021-07-07 16:56:29 +0300

    don't update instance props if not needed

--- src/field/update-helper.js
+++ src/field/update-helper.js
@@ -9,12 +9,19 @@ module.exports = function (field, values, as_is) {
   fieldSetValue.call(this, field, values, as_is)
 
   var options = fieldGetOptions.call(this, field)
-  if (options) {
+  if (options !== field.$options) {
     field.$options = options
   }
-  field.$type = (fieldDynamicType.call(this, field) || ((field.field_type === this.fieldtypes.text) && field.data_type || field.field_type)).toLowerCase()
+
+  var type = (fieldDynamicType.call(this, field) || ((field.field_type === this.fieldtypes.text) && field.data_type || field.field_type)).toLowerCase();
+  if (type !== field.$type) {
+    field.$type = type
+  }
 
   fieldValidate.call(this, field)
 
-  field.$value = fieldGetValue.call(this, field, 'label').join(field.multiline ? '<br>' : ', ')
+  var value = fieldGetValue.call(this, field, 'label').join(field.multiline ? '<br>' : ', ');
+  if (value !== field.$value) {
+    field.$value = value
+  }
 }

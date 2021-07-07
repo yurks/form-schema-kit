b2f29d56601435d924972ece1d19cb0dcd7a2f5e
Yurk Sha <yurkins@gmail.com>
2021-07-07 16:55:49 +0300

    extend `.getData()` with skip empty values and append custom data

--- src/form/get-data.js
+++ src/form/get-data.js
@@ -14,7 +14,7 @@ var stringParser = function (_i, value, name, out) {
   out.push(encodeURIComponent(name) + '=' + encodeURIComponent(value))
 }
 
-module.exports = function (type, page_id, mode) {
+module.exports = function (type, page_id, mode, skip_empty, addon) {
   var _this = this
   var out, parser
   if (type === Object) {
@@ -27,15 +27,24 @@ module.exports = function (type, page_id, mode) {
     parser = arrayParser
     out = []
   }
-  formTraverse.call(this, function (i, field) {
+  formTraverse.call(_this, function (i, field) {
     if (!field.local) {
       var values = fieldGetValue.call(_this, field, mode)
-      if (values.length === 0 && _this.fieldtypes.checkbox !== field.field_type) {
-        values = ['']
+      if (!skip_empty && values.length === 0 && _this.fieldtypes.checkbox !== field.field_type) {
+         values = ['']
       }
       each(values, parser, field.name, out)
     }
   }, page_id == null ? -Infinity : -page_id)
+
+  if (addon) {
+    each(addon, function(i, data) {
+      var value = data.value || '';
+      if (value || !skip_empty) {
+        parser(0, value, data.name, out)
+      }
+    })
+  }
 
   return type === String ? out.join('&') : out
 }
